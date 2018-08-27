b30cf216cbd1fa3886f79f0d7dad38094fafd4c0
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-27 15:19:57 -0400

    DB setup done

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO burgers (burger_name)
+VALUES ("veggie burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("black and blue burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("bacon cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("traditional burger");
