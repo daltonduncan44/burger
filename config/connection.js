811c652b98671a0167f3192543352a116b9ad602
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-27 20:38:53 -0400

    workin gof heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "root",
+		database: "burgers_db"
 
+	});
+
+};
 
 	connection = mysql.createConnection({
     host: "localhost",
@@ -10,6 +21,7 @@ var mysql = require("mysql");
 
   });
   // Make connection.
+  
     connection.connect(function(err) {
         if (err) {
           console.error("error connecting: " + err.stack);
